c40acee659eac0e1bfe94bb79bd9df4fb5ff1e97
picks6 <Zachary.picklesimer@gmail.com>
2022-03-19 02:54:50 -0400

    updated routes

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const api = require('./routes/route_api');
 const html = require('./routes/route_html');
+const path = require('path');
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', api);
-app.use('/', html);
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
 
 app.listen(PORT, () => 
     console.log(`Note Taker is running on http://localhost:${PORT}`)
